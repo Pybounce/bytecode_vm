c774b3c936c7cae214825ae74a83dba1ad76ee20
Pybounce <pybounce@protonmail.com>
2025-12-31 23:01:18 +0000

    basic exe functionality

--- examples/guessing_game.rs
+++ examples/guessing_game.rs
@@ -4,7 +4,7 @@ use bytecode_vm::interpreter::Interpreter;
 
 
 fn main() {
-    let source: String = fs::read_to_string("examples/scripts/test.dat").expect("Failed to read file");
+    let source: String = fs::read_to_string("examples/scripts/guessing_game.gart").expect("Failed to read guessing_game.gart file");
     let mut interpreter = Interpreter::new(source, Vec::new()).unwrap();
     match interpreter.run() {
         Ok(_) => (),
---
+++ src/main.rs
@@ -0,0 +1,48 @@
+
+use std::{env, fs, path::Path};
+
+use bytecode_vm::interpreter::Interpreter;
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if !(args.len() == 2 || args.len() == 3) {
+        println!("{}", args.len());
+        for arg in args {
+            println!("{}", arg);
+        }
+        println!("Unrecognised argument count.");
+        println!("Use 'cargo run -r -- --help' for a list of commands");
+        return;
+    }
+    match args[1].as_str() {
+        "--path" => {
+            if args.len() != 3 {
+                println!("Unrecognised argument count.");
+                println!("Use --help for a list of commands");
+                return;
+            }
+            run_from_path(args[2].as_str());
+        }
+        "--help" => {
+            println!("------ Usage ------");
+            println!("Use 'cargo run -r -- --path [file_path]' to run any file.");
+            println!("Use 'cargo run -r --example [example_name]' to run any built in example.");
+            println!("Use 'cargo run -r -- --help' to display this message.");
+        },
+        _ => {
+            println!("Unrecognised argument.");
+            println!("Use --help for a list of commands");
+        }
+    };
+}
+
+fn run_from_path(path: &str) {
+    let path = Path::new(path);
+    let source: String = fs::read_to_string(path).expect("Failed to read file");
+    let mut interpreter = Interpreter::new(source, Vec::new()).unwrap();
+    match interpreter.run() {
+        Ok(_) => (),
+        Err(runtime_e) => println!("{:?}", runtime_e.message),
+    }
+}
